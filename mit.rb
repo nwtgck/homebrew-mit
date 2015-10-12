d8da29bbedaa64fc47f024e14a08a69879245d5c
Ryo Ota <nwtgck@gmail.com>
2015-10-12 14:39:18 +0900

    [change] change the version

--- mit.rb
+++ mit.rb
@@ -7,7 +7,7 @@ class Mit < Formula
   homepage "https://github.com/nwtgck/homebrew-mit"
   url "https://github.com/nwtgck/homebrew-mit/raw/master/mit.jar"
   sha1 "abe88272d52532ed4cf5778a4d00a0e9bcb0e990"
-  version '0.1'
+  version '0.3'
 
   depends_on :java => '1.8+'
   # depends_on :x11 # if your formula requires any X11/XQuartz components
